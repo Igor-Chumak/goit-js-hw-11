8063cd971d4426f3de77e740021e6e485c9b89f6
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-01 09:33:10 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,180 @@
-// npm install firebase@9.23.0
-import firebase from 'firebase/compat/app';
-//// Required for side-effects
-import 'firebase/firestore';
-// Import the functions you need from the SDKs you need
+// import { getCities } from "./firebase";
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, getDocs } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { getDatabase, ref, set, child, get } from 'firebase/database';
 
-// Your web app's Firebase configuration
+//  const firebase = require('firebase');
+//  const firebaseui = require('firebaseui');
+
+//  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+// ui.start('#firebaseui-auth-container', {
+//   signInOptions: [
+//     firebase.auth.EmailAuthProvider.PROVIDER_ID
+//   ],
+//   // Other config options...
+// });
+// import { getDatabase } from "firebase/database";
+const btnUp = document.querySelector('button[data-action=signup]');
+const btnIn = document.querySelector('button[data-action=signin]');
+const form = document.querySelector('.form');
+const formBtn = document.querySelector('#formBtn');
+
+let statusAuth = 'signup';
+btnUp.style.color = 'blue';
+btnUp.style.textDecoration = 'underline';
+
+form.addEventListener('submit', onFormSubmit);
+btnUp.addEventListener('click', onBtnUpSelect);
+btnIn.addEventListener('click', onBtnInSelect);
+
+const dataUser = {
+  userId: '',
+  name: '',
+  email: '',
+  password: '',
+  authType: statusAuth,
+  data: [],
+};
+const newData = ['mango', 'poly', 'ajax', 'dinamo'];
+
+function onBtnInSelect() {
+  if (statusAuth === 'signin') {
+    return;
+  }
+
+  statusAuth = 'signin';
+  formBtn.textContent = 'SIGN IN';
+  btnIn.style.color = 'blue';
+  btnIn.style.textDecoration = 'underline';
+  btnUp.style.color = 'black';
+  btnUp.style.textDecoration = 'none';
+}
+function onBtnUpSelect() {
+  if (statusAuth === 'signup') {
+    return;
+  }
+
+  statusAuth = 'signup';
+  formBtn.textContent = 'SIGN UP';
+  btnUp.style.color = 'blue';
+  btnUp.style.textDecoration = 'underline';
+  btnIn.style.color = 'black';
+  btnIn.style.textDecoration = 'none';
+}
+
+console.log(form);
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAWL009d3fIg7FDNeFa1MpQ8vcCju1UWEQ',
   authDomain: 'project-js-fson82.firebaseapp.com',
-  databaseURL: 'https://project-js-fson82-default-rtdb.firebaseio.com',
   projectId: 'project-js-fson82',
   storageBucket: 'project-js-fson82.appspot.com',
   messagingSenderId: '729076020797',
   appId: '1:729076020797:web:e3a2da2c14a169a281dc54',
 };
 
-const refs = {};
+function onFormSubmit(event) {
+  event.preventDefault();
+  const date = new Date();
+  // dataUser.userId = date.getTime();
+  dataUser.name = form.name.value;
+  dataUser.email = form.email.value;
+  dataUser.password = form.password.value;
+  dataUser.authType = statusAuth;
+  dataUser.data = [2, 4, 5, 7, 8, 9];
+  console.log(form.email.value);
+  console.log(form.password.value);
+  console.log(dataUser);
+  form.name.value = '';
+  form.email.value = '';
+  form.password.value = '';
+  // getCities();
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const database = getDatabase();
 
-// from https://firebase.google.com/docs/firestore/quickstart?hl=ru#web-modular-api
+  if (statusAuth === 'signup') {
+    createUserWithEmailAndPassword(auth, dataUser.email, dataUser.password)
+      .then(userCredential => {
+        // Signed in
 
-import { collection, addDoc } from 'firebase/firestore';
-
-db.collection('users')
-  .add({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815,
-  })
-  .then(docRef => {
-    console.log('Document written with ID: ', docRef.id);
-  })
-  .catch(error => {
-    console.error('Error adding document: ', error);
+        const user = userCredential.user;
+        console.log(user.uid);
+        dataUser.userId = user.uid;
+        console.log(dataUser);
+        // ...
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+      });
+  } else if (statusAuth === 'signin') {
+    signInWithEmailAndPassword(auth, dataUser.email, dataUser.password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user.uid);
+        dataUser.userId = user.uid;
+        console.log(dataUser);
+        readUserData(dataUser.userId);
+        // ...
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        alert(errorMessage);
+      });
+  }
+  setTimeout(() => {
+    writeUserData(dataUser);
+  }, 5000);
+  // setTimeout(() => {
+  //     updateUserData(newData,dataUser.userId);
+  // },8000)
+}
+function updateUserData(array, userId) {
+  const db = getDatabase();
+  return update(ref(db, 'users/' + userId), {
+    data: array,
+    username: 'User',
+    // profile_picture : imageUrl
   });
+}
 
-// try {
-//   const docRef = await addDoc(collection(db, 'users'), {
-//     first: 'Ada',
-//     last: 'Lovelace',
-//     born: 1815,
-//   });
-//   console.log('Document written with ID: ', docRef.id);
-// } catch (e) {
-//   console.error('Error adding document: ', e);
-// }
+function writeUserData({ userId, name, email, password, data }) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    password: password,
+    data: data,
+    // profile_picture : imageUrl
+  });
+}
 
-// test
-newMessageRef = {
-  name: 'Igor',
-  email: 'chigorv@ukr.net',
-  subject: 'subject test',
-  message: '1234567890',
-};
-
-// Reference messages collection
-// let messagesRef = firebase.database().ref('contactformmessages');
-
-// $('#contactForm').submit(function (e) {
-//   e.preventDefault();
-
-//   var newMessageRef = messagesRef.push();
-//   newMessageRef.set({
-//     name: $('.fullname').val(),
-//     email: $('.email').val(),
-//     subject: $('.subject').val(),
-//     message: $('.message').val(),
-//   });
-
-//   $('.success-message').show();
-
-//   $('#contactForm')[0].reset();
-// });
+function readUserData(userId) {
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `users/${userId}`))
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log('No data available');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+export { dataUser };
